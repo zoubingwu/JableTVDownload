e79f10dfd6cffe3e3110b8b1445488164dae64e0
zoubingwu <zoubingwu@gmail.com>
2023-08-11 16:41:01 +0800

    args change

--- main.py
+++ main.py
@@ -1,26 +1,28 @@
 # author: hcjohn463
 #!/usr/bin/env python
 # coding: utf-8
-from args import *
+import args
 from download import download
 from movies import movieLinks
+
 # In[2]:
 
-parser = get_parser()
+parser = args.get_parser()
 args = parser.parse_args()
 
-if(len(args.url) != 0):
-    url = args.url
+if len(args.url) != 0:
+    urls = args.url.split(",")
+    for url in urls:
+        download(url)
+elif args.random is True:
+    url = args.av_recommand()
     download(url)
-elif(args.random == True):
-    url = av_recommand()
-    download(url)
-elif(args.all_urls != ""):
+elif args.all_urls != "":
     all_urls = args.all_urls
     urls = movieLinks(all_urls)
     for url in urls:
         download(url)
 else:
     # 使用者輸入Jable網址
-    url = input('輸入jable網址:')
+    url = input("輸入jable網址:")
     download(url)
